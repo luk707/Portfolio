713355df833b8582aed8681da2444ec939449c9e
Luke Harris <lukeharris01170@gmail.com>
2018-10-27 12:57:55 +0100

    Add container to page 2

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Layout from '../components/layout'
 import { LinkButton } from '../components/button'
+import Container from '../component/container'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <LinkButton prominence="primary" to="/">
-      Go back to the homepage
-    </LinkButton>
+    <Container>
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <LinkButton prominence="primary" to="/">
+        Go back to the homepage
+      </LinkButton>
+    </Container>
   </Layout>
 )
 
