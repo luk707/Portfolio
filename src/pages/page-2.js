5ad9de2e88a8405589dc5114112bcbf34069ac88
Luke Harris <lukeharris01170@gmail.com>
2018-10-27 13:12:23 +0100

    Fix typo

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
 import { LinkButton } from '../components/button'
-import Container from '../component/container'
+import Container from '../components/container'
 
 const SecondPage = () => (
   <Layout>
