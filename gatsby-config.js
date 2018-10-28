d0f3187bf680067fcd4cfe0d1ea9f47a2a9d8bc4
Luke Harris <lukeharris01170@gmail.com>
2018-10-28 15:26:21 +0000

    Add netlify cms config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     'gatsby-plugin-emotion',
     'gatsby-plugin-flow',
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-netlify-cms',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
