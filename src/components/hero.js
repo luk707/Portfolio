d32aa92f3181d4ef0ff2cb18125977d1813e84d0
Luke Harris <lukeharris01170@gmail.com>
2018-11-01 12:38:54 +0000

    Fallback to solid color before hero background has loaded

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,11 +1,15 @@
+// @flow
+
 import styled from 'react-emotion'
 import background from '../../static/img/hero-background.svg'
+import { Theme } from '../theme'
 
-const Hero = styled('div')({
+const Hero = styled('div')((props: { theme: Theme }) => ({
   backgroundImage: `url(${background})`,
   backgroundRepeat: 'no-repeat',
   backgroundSize: 'cover',
   backgroundPosition: 'center center',
-})
+  backgroundColor: props.theme.colorPrimary,
+}))
 
 export default Hero
