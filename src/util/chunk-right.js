01a39737007c8f2c3ac7037eb5da22eaae755238
Luke Harris <lukeharris01170@gmail.com>
2018-12-13 08:34:51 +0000

    Implement chunkRight function

--- src/util/chunk-right.js
+++ src/util/chunk-right.js
@@ -1,8 +1,18 @@
 // @flow
 
+import takeRight from 'lodash/takeRight'
+
 export default function chunkRight<T>(
   values: Array<T>,
   chunkSize: number
 ): Array<Array<T>> {
-  return [[]]
+  const buffer = [...values]
+  const result = []
+  while (buffer.length > chunkSize) {
+    result.push(takeRight(buffer, chunkSize))
+    buffer.splice(-chunkSize, chunkSize)
+  }
+  result.push(buffer)
+  result.reverse()
+  return result
 }
